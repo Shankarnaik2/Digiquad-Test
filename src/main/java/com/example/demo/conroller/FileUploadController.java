8ec41620e1000cb5f564a85878063c00b54b6690
Shankarnaik2 <140405202+Shankarnaik2@users.noreply.github.com>
2024-03-04 13:16:47 +0530

    changes

--- src/main/java/com/example/demo/conroller/FileUploadController.java
+++ src/main/java/com/example/demo/conroller/FileUploadController.java
@@ -8,12 +8,11 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
 import com.example.demo.service.FileService;
 
-@RestController
+
 @Controller
 public class FileUploadController {
     
